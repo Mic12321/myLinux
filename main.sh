e22fe749e5237ee17d0d8ed8c1f4ee76afe5a282
HPN <huipokngai1124@gmail.com>
2022-12-02 02:42:50 +0000

    Added a few changes

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 echo "Running main.sh"
+echo "Please enter your superuser password to continue"
 
-echo "upating and upgrading packages from apt"
+sudo echo "upating and upgrading packages from apt"
 sudo apt update
 sudo apt upgrade -y
 echo "Finish updating and upgrading"
@@ -11,8 +12,8 @@ sudo apt-get install gparted -y
 sudo apt install git-all
 sudo apt install kate -y
 sudo apt intall g++ -y
-sudo apt install htop
-sudo apt install dpkg
+sudo apt install htop -y
+sudo apt install dpkg -y
 echo "Finish all processes from apt"
 
 echo "Updating softwares from snap"
@@ -20,10 +21,9 @@ sudo snap refresh
 echo "Finish updating"
 
 echo "Downloading and installing softwares from snap"
-
-sudo snap install eclipse --classic -y
-sudo snap install netbeans --classic -y
 sudo snap install signal-desktop
 sudo snap install code
 echo "Finish all processes from snap"
-echo "Program end"
+echo "Program end. The following applications are required to install mannually."
+echo "sudo snap install eclipse --classic"
+echo "sudo snap install netbeans --classic"
